0a34af3d538bbc278ad94497b1f6a59a9d058957
Vatsal6288 <kachhadiyavatsal1@gmail.com>
2026-08-14 17:10:44 +0530

    refactor(button): added download button

---
+++ src/components/layout/Download.tsx
@@ -0,0 +1,14 @@
+// Component Imports
+import { Button } from '@/components/ui/button'
+
+const BuyNowButton = () => {
+  return (
+    <Button asChild className='animate-heartbeat fixed right-15 bottom-8 z-70'>
+      <a href='https://shadcnstudio.com/templates/ink-blog-template-free' target='_blank'>
+        Download
+      </a>
+    </Button>
+  )
+}
+
+export default BuyNowButton
